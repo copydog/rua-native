ba03eed8729b6a81668599c254d751f89076a6fa
copydog <775150@qq.com>
2018-07-01 22:17:00 +0800

    chore: update dep

--- .webpackrc.js
+++ .webpackrc.js
@@ -1,16 +1,28 @@
 const theme = require('./web/theme')
+require('./scripts/fix/index')
 
 module.exports = {
-  entry: 'index.js',
+  entry: 'ts/index.tsx',
   alias: {
     'react-native': 'react-native-web'
   },
-  extraBabelPlugins: [],
-  extraBabelIncludes: [],
+  extraBabelPlugins: [
+    '@babel/transform-flow-strip-types',
+    'react-native-web'
+  ],
+  extraBabelPresets: [
+    '@babel/preset-env',
+    '@babel/preset-flow',
+  ],
+  extraBabelIncludes: [
+    'node_modules/react-navigation',
+    'node_modules/react-navigation-tabs',
+    'node_modules/react-native-safe-area-view',
+  ],
   theme,
   disableDynamicImport: true,
   hash: true,
   html: {
-    template: './web/index.ejs',
+    template: 'web/index.ejs',
   },
 }
